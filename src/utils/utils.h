4ab80593d3236d1aa784bc59c3169e77943875dd
jgo-42 <jingmuio@gmail.com>
2023-10-10 17:13:22 +0900

    [WWM-43] style: apply clang-format

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,6 @@
 #pragma once
 #ifndef UTILS_H
-# define UTILS_H
+#define UTILS_H
 
 #include <iostream>
 #include <sstream>
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -1,11 +1,11 @@
 #pragma once
 #ifndef UTILS_HPP
-# define UTILS_HPP
+#define UTILS_HPP
 
 #include "utils.h"
 
 namespace utils {
-    std::string itos(const int& num);
+	std::string itos(const int& num);
 }
 
 #endif
