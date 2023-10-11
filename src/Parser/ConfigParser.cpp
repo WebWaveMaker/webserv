da84834d4e6c79db9a7e641d79b50b9616f53f50
ekwak <ekwak@student.42seoul.kr>
2023-10-12 03:34:55 +0900

    [WWM-31] feat(Parser): ConfigParser 클래스 및 메소드 추가
    ConfigParser 클래스는 설정 파일을 파싱하기 위한 클래스입니다. 해당 커밋에서는 ConfigParser의 메소드들과 관련 헤더 파일을 추가했습니다.

---
+++ src/Parser/ConfigParser.cpp
@@ -0,0 +1,150 @@
+// ConfigParser.cpp
+#include "ConfigParser.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+ConfigParser::ConfigParser() {}
+
+ConfigParser::~ConfigParser() {}
+
+bool ConfigParser::parse(const std::string& filename, ConfigStore& config) {
+	std::ifstream infile(filename.c_str());
+	if (infile.is_open() == false) {
+		throw "Could not open file: " + filename;
+	}
+
+	std::string content((std::istreambuf_iterator<char>(infile)), (std::istreambuf_iterator<char>()));
+
+	size_t position = 0;
+	HttpBlock http = HttpBlock();
+	bool t = this->httpBlockTokenizer(content, position, http);
+	config.setServerConfig(http);
+	return t;
+}
+
+void ConfigParser::skipWhitespace(const std::string& content, size_t& position) {
+	while (position < content.size() && std::isspace(content[position])) {
+		position++;
+	}
+}
+
+bool ConfigParser::match(const std::string& content, size_t& position, const std::string& expected) {
+	this->skipWhitespace(content, position);
+
+	size_t expectedLength = expected.size();
+	if (position + expectedLength <= content.size() && content.substr(position, expectedLength) == expected) {
+		size_t endPosition = position + expectedLength;
+		if (endPosition == content.size() || std::isalnum(content[endPosition]) == false ||
+			content[endPosition] == ';') {
+			position += expectedLength;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool ConfigParser::directiveTokenizer(const std::string& content, size_t& position, Directive& directive) {
+	this->skipWhitespace(content, position);
+	while (position < content.size() && std::isspace(content[position]) == false && content[position] != ';') {
+		directive.name.push_back(content[position]);
+		position++;
+	}
+
+	this->skipWhitespace(content, position);
+	while (position < content.size() && content[position] != ';') {
+		std::string parameter;
+		while (position < content.size() && std::isspace(content[position]) == false && content[position] != ';') {
+			parameter.push_back(content[position]);
+			position++;
+		}
+		directive.parameters.push_back(parameter);
+		this->skipWhitespace(content, position);
+	}
+
+	return this->match(content, position, ";");
+}
+
+bool ConfigParser::locationBlockTokenizer(const std::string& content, size_t& position, LocationBlock& locationBlock) {
+	// locationBlockTokenizer는 location 블록을 파싱합니다.
+	this->skipWhitespace(content, position);
+	size_t startPos = position;
+	while (position < content.size() && content[position] != '{')
+		position++;
+
+	locationBlock.identifier = content.substr(startPos, position - startPos);
+	this->skipWhitespace(content, position);  // 이 부분을 추가하여 공백을 건너뜁니다.
+
+	if (this->match(content, position, "{") == false)
+		return false;
+
+	while (position < content.size()) {
+		this->skipWhitespace(content, position);
+		if (this->match(content, position, "}")) {
+			// 종료 중괄호를 찾으면 루프를 종료합니다.
+			return true;
+		} else {
+			Directive directive;
+			if (!this->directiveTokenizer(content, position, directive))
+				return false;
+			locationBlock.directives.push_back(directive);
+		}
+	}
+
+	return false;  // 블록의 종료를 찾지 못하면 false를 반환합니다.
+}
+
+bool ConfigParser::serverBlockTokenizer(const std::string& content, size_t& position, ServerBlock& serverBlock) {
+	if (this->match(content, position, "{") == false)
+		return false;
+
+	while (position < content.size()) {
+		this->skipWhitespace(content, position);
+		if (this->match(content, position, "}")) {
+			// 종료 중괄호를 찾으면 루프를 종료합니다.
+			return true;
+		} else if (this->match(content, position, "location")) {
+			LocationBlock locationBlock;
+			if (this->locationBlockTokenizer(content, position, locationBlock) == false)
+				return false;
+			serverBlock.locations.push_back(locationBlock);
+		} else {
+			Directive directive;
+			if (this->directiveTokenizer(content, position, directive) == false)
+				return false;
+			serverBlock.directives.push_back(directive);
+		}
+	}
+
+	return false;  // 블록의 종료를 찾지 못하면 false를 반환합니다.
+}
+
+bool ConfigParser::httpBlockTokenizer(const std::string& content, size_t& position, HttpBlock& httpBlock) {
+	if (this->match(content, position, "http") == false) {
+		throw "Expected 'http' keyword at position: " + std::to_string(position);
+	}
+
+	if (this->match(content, position, "{") == false) {
+		throw "Expected '{' at position: " + std::to_string(position);
+	}
+
+	while (position < content.size() && this->match(content, position, "}") == false) {
+		if (this->match(content, position, "server")) {
+			ServerBlock serverBlock;
+			if (this->serverBlockTokenizer(content, position, serverBlock) == false) {
+				throw "Failed to parse server block inside http block at position: " + std::to_string(position);
+			}
+			httpBlock.servers.push_back(serverBlock);
+		} else {
+			Directive directive;
+			if (this->directiveTokenizer(content, position, directive) == false) {
+				throw "Failed to parse directive inside http block at position: " + std::to_string(position);
+			}
+			httpBlock.directives.push_back(directive);
+		}
+	}
+
+	return true;
+}
