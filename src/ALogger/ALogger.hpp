a89413e5491989eb098419e4262faf173cbfa227
JinMu Go <jingmuio@gmail.com>
2023-10-12 11:34:10 +0900

    [WWM-6] fix(ALogger): change pure method prototype
    AccessLogger와 ErrorLogger의 동작을 위해 ALogger의 pure method를 변경하였습니다.

--- src/ALogger/ALogger.hpp
+++ src/ALogger/ALogger.hpp
@@ -31,8 +31,8 @@ class ALogger {
 	static std::string getCurTime(const char* format);
 
 	// pure method
-	virtual std::string makeLogMsg(const std::string& msg, const std::string& func) = 0;
-	virtual void log(const std::string& msg, const char* func, const int arg) = 0;
+	virtual std::string makeLogMsg(const std::string& msg, const std::string& func, void* arg) = 0;
+	virtual void log(const std::string& msg, const char* func, const int enum__, void* arg) = 0;
 };
 
 #endif
