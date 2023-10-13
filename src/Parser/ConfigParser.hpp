9a4c632025edbc2d308cbb33ee8d3108866201c1
ekwak <ekwak@student.42seoul.kr>
2023-10-14 00:11:18 +0900

    [WWM-31] refactor(ConfigParser): ConfigParser 클래스 임시 수정
    ConfigParser 클래스를 테스트 목적으로 일시적으로 수정하였습니다.
    - ConfigStore 대신 CommonConfig를 일시적으로 사용합니다.
    - serverBlockTokenizer와 locationBlockTokenizer 함수를 임시로 주석 처리하였습니다.
    
    이 변경사항은 테스트 과정 중 임시로 적용되며, 최종 테스트 완료 후에는 원래의 구조로 복귀될 예정입니다.

--- src/Parser/ConfigParser.hpp
+++ src/Parser/ConfigParser.hpp
@@ -3,21 +3,23 @@
 #ifndef CONFIG_PARSER_H
 #define CONFIG_PARSER_H
 
-#include "ConfigStore.hpp"
+// #include "ConfigStore.hpp"
+#include "CommonConfig.hpp"
 #include "IParser.hpp"
+#include "Parser.h"
 
-class ConfigParser : public IParser<ConfigStore> {
+class ConfigParser : public IParser<CommonConfig> {
    public:
 	ConfigParser();
 	virtual ~ConfigParser();
-	virtual bool parse(const std::string& filename, ConfigStore& configStore) override;
+	virtual bool parse(const std::string& filename, CommonConfig& http);
 
    private:
 	bool match(const std::string& content, size_t& position, const std::string& expected);
 	void skipWhitespace(const std::string& content, size_t& position);
 	bool httpBlockTokenizer(const std::string& content, size_t& position, HttpBlock& httpBlock);
-	bool serverBlockTokenizer(const std::string& content, size_t& position, ServerBlock& serverBlock);
-	bool locationBlockTokenizer(const std::string& content, size_t& position, LocationBlock& locationBlock);
+	// bool serverBlockTokenizer(const std::string& content, size_t& position, ServerBlock& serverBlock);
+	// bool locationBlockTokenizer(const std::string& content, size_t& position, LocationBlock& locationBlock);
 	bool directiveTokenizer(const std::string& content, size_t& position, Directive& directive);
 };
 
