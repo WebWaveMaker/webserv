f364c1c2c991b144d25cab06f8e5b497913a9db7
ekwak <ekwak@student.42seoul.kr>
2023-10-14 00:16:33 +0900

    [WWM-31] refactor(Parser): CommonConfig 클래스 변경
    CommonConfig 클래스의 구조가 변경되었습니다.
    - "ConfigValue.hpp" 대신 "IConfig.hpp"를 포함합니다.
    - CommonConfig는 이제 IConfig 인터페이스를 상속받습니다.
    - 개별 설정 값을 저장하는 private 멤버 변수들은 _values map으로 통합되었습니다.
    - 이에 따라 관련 getter 및 setter 함수들이 수정되었습니다.
    
    이 변경사항은 코드의 유지 및 관리를 향상시키기 위해 진행되었습니다.

--- src/Parser/CommonConfig.hpp
+++ src/Parser/CommonConfig.hpp
@@ -2,42 +2,24 @@
 #ifndef COMMONCONFIG_HPP
 #define COMMONCONFIG_HPP
 
-#include "ConfigValue.hpp"
+#include "IConfig.hpp"
 #include "Parser.h"
 
-class CommonConfig {
+class CommonConfig : public IConfig<HttpDirectives, void*> {
    private:
-	ConfigValue _sendFile;
-	ConfigValue _keepAliveTimeout;
-	ConfigValue _defaultType;
-	ConfigValue _clientMaxBodySize;
-	ConfigValue _errorLog;
+	std::map<HttpDirectives, ConfigValue> _values;	  // 각 HTTP 메소드에 대응하는 설정 값들을 저장
 	std::map<unsigned int, std::string> _errorPages;  // 오류 페이지들을 저장
-
-	// 각 private 맴버 변수에 대응하는 getter 함수들
-	bool getSendFile() const;
-	unsigned int getKeepAliveTimeout() const;
-	std::string getDefaultType() const;
-	unsigned int getClientMaxBodySize() const;
-
-	// 각 private 맴버 변수에 대응하는 setter 함수들
-	void setSendFile(const std::string& value);
-	void setKeepAliveTimeout(const std::string& value);
-	void setDefaultType(const std::string& value);
-	void setClientMaxBodySize(const std::string& value);
 
    public:
 	CommonConfig();
 	CommonConfig(const CommonConfig& other);
-	virtual ~CommonConfig();
+	~CommonConfig();
 	CommonConfig& operator=(const CommonConfig& other);
 
-	void setErrorLog(const std::string& logPath, const std::string& logLevel);
 	void setErrorPage(const std::vector<std::string>& values);
-	void setValueByHttpMethod(const std::string& directive, const std::vector<std::string>& values);
-	std::pair<std::string, LogLevels> getErrorLog() const;
+	void setDirectives(const std::string& directive, const std::vector<std::string>& values);
 	std::string getErrorPage(unsigned int error_code) const;
-	ConfigValue getValueByHttpMethod(HttpDirectives method) const;
+	ConfigValue getDirectives(HttpDirectives method) const;
 };
 
 #endif	// COMMONCONFIG_HPP
