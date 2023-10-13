29f85ffb4ad4da98d1cea09dc49e3f6e27318a8e
ekwak <ekwak@student.42seoul.kr>
2023-10-14 00:18:01 +0900

    [WWM-31] feat(Parser): ConfigStore 및 ServerConfig 클래스 변경
    ConfigStore 클래스에 대한 새로운 구현 및 메소드 수정이 이루어졌습니다. 이와 함께, ServerConfig에서 일부 메소드와 변수들이 제거되었습니다.

--- src/Parser/ConfigStore.hpp
+++ src/Parser/ConfigStore.hpp
@@ -2,16 +2,25 @@
 #ifndef CONFIGSTORE_HPP
 #define CONFIGSTORE_HPP
 
-#include <vector>
+#include "CommonConfig.hpp"
+#include "Parser.h"
 #include "ServerConfig.hpp"
 
 class ConfigStore {
    private:
-	std::vector<ServerConfig> _servers;
+	CommonConfig _common;
+	// std::vector<ServerConfig> _servers;
 
    public:
 	ConfigStore();
+	ConfigStore(const CommonConfig& common);
 	ConfigStore(const ConfigStore& obj);
 	~ConfigStore();
 	ConfigStore& operator=(const ConfigStore& obj);
+
+	// void addServer(const ServerConfig& server);
+	CommonConfig& getCommonConfig();
+	// std::vector<ServerConfig>& getServers();
 };
+
+#endif	// CONFIGSTORE_HPP
