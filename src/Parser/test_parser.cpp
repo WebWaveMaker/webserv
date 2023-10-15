0c9d77ee132011e36eaf10f949c86349250770e0
ekwak <ekwak@student.42seoul.kr>
2023-10-16 00:38:21 +0900

    [WWM-31] test(Parser): 테스트 출력 로직 수정 및 활성화
    기존에 주석 처리된 파싱 테스트를 활성화하였습니다. 또한 서버의 제한된 HTTP 메소드를 출력하는 로직을 추가하였습니다.

--- src/Parser/test_parser.cpp
+++ src/Parser/test_parser.cpp
@@ -8,7 +8,7 @@ int main() {
 	std::vector<ServerConfig> servers;
 	bool result = parser.parse("test_config.txt", servers);
 
-	// assert(result && "Parsing failed.");
+	assert(result && "Parsing failed.");
 
 	// Check if it parsed 2 server configurations
 	// assert(servers.size() == 2 && "Expected 2 server configurations.");
@@ -17,16 +17,42 @@ int main() {
 	std::cout << "Server 1:" << server1.getDirectives(ServerDirectives::SERVER_LISTEN).asUint() << std::endl;
 	std::cout << "Server 1:" << server1.getDirectives(ServerDirectives::SERVER_SERVER_NAME).asString() << std::endl;
 	std::cout << "Server 1:" << server1.getDirectives(ServerDirectives::SERVER_ROOT).asString() << std::endl;
-	// std::cout << "Server 1:" << server1.getDirectives(ServerDirectives::SERVER_LIMIT_EXCEPT).asMedVec() << std::endl;
-	std::cout << "Server 1:" << server1.getDirectives(ServerDirectives::SERVER_INDEX).asStrVec()[0] << std::endl;
+	std::vector<enum HttpMethod> server_limit_except =
+		server1.getDirectives(ServerDirectives::SERVER_LIMIT_EXCEPT).asMedVec();
+	for (std::vector<enum HttpMethod>::iterator it = server_limit_except.begin(); it != server_limit_except.end();
+		 ++it) {
+		if (*it == HttpMethod::GET) {
+			std::cout << "Server 1: GET" << std::endl;
+		} else if (*it == HttpMethod::POST) {
+			std::cout << "Server 1: POST" << std::endl;
+		} else if (*it == HttpMethod::DELETE) {
+			std::cout << "Server 1: DELETE" << std::endl;
+		} else if (*it == HttpMethod::PUT) {
+			std::cout << "Server 1: PUT" << std::endl;
+		}
+	}
+	std::cout << "Server 1:" << server1.getDirectives(ServerDirectives::SERVER_INDEX).asString() << std::endl;
 	std::cout << "Server 1:" << server1.getDirectives(ServerDirectives::SERVER_AUTOINDEX).asBool() << std::endl;
 
 	ServerConfig& server2 = servers[1];
 	std::cout << "Server 2:" << server2.getDirectives(ServerDirectives::SERVER_LISTEN).asUint() << std::endl;
 	std::cout << "Server 2:" << server2.getDirectives(ServerDirectives::SERVER_SERVER_NAME).asString() << std::endl;
 	std::cout << "Server 2:" << server2.getDirectives(ServerDirectives::SERVER_ROOT).asString() << std::endl;
-	// std::cout << "Server 1:" << server2.getDirectives(ServerDirectives::SERVER_LIMIT_EXCEPT).asMedVec() << std::endl;
-	std::cout << "Server 2:" << server2.getDirectives(ServerDirectives::SERVER_INDEX).asStrVec()[0] << std::endl;
+	std::vector<enum HttpMethod> server_limit_except2 =
+		server2.getDirectives(ServerDirectives::SERVER_LIMIT_EXCEPT).asMedVec();
+	for (std::vector<enum HttpMethod>::iterator it = server_limit_except2.begin(); it != server_limit_except2.end();
+		 ++it) {
+		if (*it == HttpMethod::GET) {
+			std::cout << "Server 2: GET" << std::endl;
+		} else if (*it == HttpMethod::POST) {
+			std::cout << "Server 2: POST" << std::endl;
+		} else if (*it == HttpMethod::DELETE) {
+			std::cout << "Server 2: DELETE" << std::endl;
+		} else if (*it == HttpMethod::PUT) {
+			std::cout << "Server 2: PUT" << std::endl;
+		}
+	}
+	std::cout << "Server 2:" << server2.getDirectives(ServerDirectives::SERVER_INDEX).asString() << std::endl;
 	std::cout << "Server 2:" << server2.getDirectives(ServerDirectives::SERVER_AUTOINDEX).asBool() << std::endl;
 
 	std::cout << "All tests passed!" << std::endl;
