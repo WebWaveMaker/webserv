69328c6f14d2e56f8626d2095985635dbbb789f5
idealflower-k <aekrcu@naver.com>
2023-10-16 20:44:19 +0900

    [WWM-58] feat: draft Client class
    Client class의 초안을 작성했습니다.
    추후에 req, res, eventHandler의 추가가 필요합니다.

---
+++ src/Client/Client.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <sys/event.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <exception>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#endif
