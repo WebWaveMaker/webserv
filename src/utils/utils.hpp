24e076e596addcf205187d358327e09b34d41f8e
jgo-42 <jingmuio@gmail.com>
2023-10-16 21:25:48 +0900

    [WWM-56] feat(TSingleton): implement Singleton Class
    클래스하나당 인스턴스 하나를 관리하기 위해 싱글턴 클래스를
    구현하였습니다.

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -6,6 +6,38 @@
 
 namespace utils {
 	std::string itos(const int& num);
-}
+
+	template <class T>
+	class TSingleton {
+
+	   public:
+		static T* getInstance() {
+			if (_instance == nullptr_t) {
+				_instance = new T;
+				std::atexit(deleteInstance);
+			}
+			return _instance;
+		}
+
+	   protected:
+		TSingleton() {}
+		virtual ~TSingleton() {}
+
+		static void deleteInstance() {
+			if (_instance != nullptr_t)
+				delete _instance;
+			_instance = nullptr_t;
+		}
+
+	   private:
+		static T* _instance;
+	};
+
+	template <class T>
+	T* TSingleton<T>::_instance = nullptr_t;
+
+}  // namespace utils
+
+namespace u = utils;
 
 #endif
