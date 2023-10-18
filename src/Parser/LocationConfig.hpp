a69edc7f7d581bc34529156cb8bdf378d25723d0
ekwak <ekwak@student.42seoul.kr>
2023-10-18 11:15:44 +0900

    [WWM-31] refactor(Parser): LocationConfig 클래스 구조 개선
    LocationConfig 클래스에서 구조와 메서드를 리팩토링하였습니다. 특히, `AConfig`에서 상속받도록 변경하고 관련 함수들의 시그니처가 변경되었습니다. 이전의 여러 private getter와 setter 함수들이 최적화 되었으며, 새로운 virtual 함수들이 추가되었습니다.
    
    다양한 구성 요소의 가독성과 관리를 향상시키기 위해 이러한 변경이 필요하였습니다.

--- src/Parser/LocationConfig.hpp
+++ src/Parser/LocationConfig.hpp
@@ -5,43 +5,23 @@
 #include "Parser.h"
 #include "ServerConfig.hpp"
 
-class LocationConfig {
+class ServerConfig;
+
+class LocationConfig : public AConfig {
    private:
-	ServerConfig* _serverConfig;  // Pointer to ServerConfig
-
-	bool _sendFile;
-	std::map<int, std::string> _errorPages;
-	std::vector<HttpMethod> _limitExcept;
-	std::string _root;
-	bool _autoIndex;
-	std::string _index;
-
-	// Private Getters
-	bool getSendFile() const;
-	std::vector<HttpMethod> getLimitExcept() const;
-	std::string getRoot() const;
-	bool getAutoIndex() const;
-	std::string getIndex() const;
-
-	// Private Setters
-	void setSendFile(const std::string& value);
-	void setErrorPage(const std::string& errorCode, const std::string& errorPage);
-	void setLimitExcept(const std::string& value);
-	void setRoot(const std::string& value);
-	void setAutoIndex(const std::string& value);
-	void setIndex(const std::string& value);
-	void setValueByLocationDirectives(LocationDirectives method, const std::string& value);
+	ServerConfig* _parent;
 
    public:
-	LocationConfig(ServerConfig* serverConfig);
-	virtual ~LocationConfig();	// Specify virtual for the destructor
-
-	// Copy constructor & assignment operator
+	LocationConfig();
+	LocationConfig(ServerConfig* parent);  // Moved initialization to constructor initializer list
 	LocationConfig(const LocationConfig& other);
+	~LocationConfig();
 	LocationConfig& operator=(const LocationConfig& other);
 
-	std::string getErrorPage(int errorCode) const;
-	ConfigValue getValueByLocationDirectives(LocationDirectives method) const;
+	virtual void setDirectives(const std::string& directive, const std::vector<std::string>& values);
+	virtual void setErrorPage(const std::vector<std::string>& values);
+	virtual std::string getErrorPage(unsigned int error_code) const;
+	virtual ConfigValue getDirectives(Directives method) const;
 };
 
 #endif	// LOCATIONCONFIG_HPP
