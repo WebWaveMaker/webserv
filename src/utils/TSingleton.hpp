1c62ba5fcb7c7d8dab0cd3c271bf3c83076634c5
jgo-42 <jingmuio@gmail.com>
2023-10-20 11:40:21 +0900

    [WWM-42] feat(utils): divide util class
    utils.hpp 파일에 모두 구현되었던 nullptr_t, TSingleton class들을 각각의
    파일로 나누어 작성하였습니다.

---
+++ src/utils/TSingleton.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef TSINGLETON_HPP
+#define TSINGLETON_HPP
+
+#include "utils.h"
+
+namespace utils {
+
+	template <class T>
+	class TSingleton {
+
+	   public:
+		static T* getInstance() {
+			if (_instance == nullptr_t) {
+				_instance = new T;
+				std::atexit(deleteInstance);
+			}
+			return _instance;
+		}
+
+	   protected:
+		TSingleton() {}
+		virtual ~TSingleton() {}
+
+		static void deleteInstance() {
+			if (_instance != nullptr_t)
+				delete _instance;
+			_instance = nullptr_t;
+		}
+
+	   private:
+		static T* _instance;
+	};
+
+	template <class T>
+	T* TSingleton<T>::_instance = nullptr_t;
+
+}  // namespace utils
+
+#endif
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -7,51 +7,6 @@
 namespace utils {
 	std::string itos(const int& num);
 
-	const class nullptr_t {
-	   public:
-		template <typename T>
-		operator T*() const {
-			return 0;
-		}
-		template <typename C, typename T>
-		operator T C::*() const {
-			return 0;
-		}
-
-	   private:
-		void operator&() const;
-
-	} nullptr_t = {};
-
-	template <class T>
-	class TSingleton {
-
-	   public:
-		static T* getInstance() {
-			if (_instance == nullptr_t) {
-				_instance = new T;
-				std::atexit(deleteInstance);
-			}
-			return _instance;
-		}
-
-	   protected:
-		TSingleton() {}
-		virtual ~TSingleton() {}
-
-		static void deleteInstance() {
-			if (_instance != nullptr_t)
-				delete _instance;
-			_instance = nullptr_t;
-		}
-
-	   private:
-		static T* _instance;
-	};
-
-	template <class T>
-	T* TSingleton<T>::_instance = nullptr_t;
-
 }  // namespace utils
 
 namespace u = utils;
