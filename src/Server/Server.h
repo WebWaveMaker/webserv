034531d956ca66b7d8c08c1602e2057b6699315c
idealflower-k <aekrcu@naver.com>
2023-10-25 20:42:38 +0900

    [WWM-13] chore: clean headerfile in Server

--- src/Server/Server.h
+++ src/Server/Server.h
@@ -3,34 +3,18 @@
 #define SERVER_H
 
 #include <arpa/inet.h>
-#include <fcntl.h>
-#include <sys/event.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-#include <exception>
+#include <cstring>	// for std::memset
 #include <iostream>
 #include <map>
-#include <vector>
+#include <stdexcept>  // for std::runtime_error
 
 #include "AccessLogger.hpp"
 #include "Client.hpp"
+#include "ClientEventHandler.hpp"
 #include "Dispatcher.hpp"
 #include "ErrorLogger.hpp"
 #include "ICallback.hpp"
 #include "ServerConfig.hpp"
 #include "ServerEventHandler.hpp"
-#include "TSingleton.hpp"
-
-class ServerConfig;
-class Client;
-class ServerEventHandler;
-class AccessLogger;
-class ErrorLogger;
-class ClientEventHandler;
-
-enum EventType;
 
 #endif
