75a588b970371e35a02a43387a8056aa5ca97077
idealflower-k <aekrcu@naver.com>
2023-10-25 20:44:07 +0900

    [WWM-13] feat: add ICallback constructor

--- src/Server/ICallback.hpp
+++ src/Server/ICallback.hpp
@@ -8,9 +8,9 @@ class ICallback {
    private:
 	ICallback& operator=(const ICallback& obj);
 	ICallback(const ICallback& obj);
-	ICallback();
 
    public:
+	ICallback();
 	virtual void eraseClient(int key) = 0;
 	virtual Client* createClient(int clientFd, struct sockaddr_in& clientAddr) = 0;
 	virtual ICallback* getCallback() = 0;
