611ea2a42c35e1085c06734b26e33ea4430ff4ea
idealflower-k <aekrcu@naver.com>
2023-10-30 17:31:26 +0900

    [WWM-80] feat: can management multi AEvent on fd_t
    Dispatcher에서 하나의 fd에 대해서 여러개의 AEventHandler를 관리 하도록
    변경했습니다.

--- src/Reactor/Dispatcher/Dispatcher.cpp
+++ src/Reactor/Dispatcher/Dispatcher.cpp
@@ -3,22 +3,31 @@
 namespace reactor {
 
 	Dispatcher::Dispatcher() : _demultiplexer(SyncEventDemultiplexer::getInstance()) {}
-	Dispatcher::~Dispatcher() {}
-	void Dispatcher::registerHander(u::shared_ptr<AEventHandler> handler, enum EventType type) {
-		const handle_t handle = handler->getHandle();
-		const std::map<handle_t, u::shared_ptr<AEventHandler> >::iterator cit = _handlers.find(handle);
 
-		if (cit == _handlers.end()) {
-			_handlers[handle] = handler;
+	Dispatcher::~Dispatcher() {}
+
+	void Dispatcher::registerHander(u::shared_ptr<AEventHandler> handler, enum EventType type) {
+		const fd_t handle = handler->getHandle();
+
+		if (this->_handlers.find(handle) == this->_handlers.end()) {
+			this->_handlers[handle].push_back(handler);
+			this->_handlerIndices[handler] = this->_handlers[handle].size() - 1;
 		}
-		_demultiplexer->requestEvent(handler.get(), type);
+		this->_demultiplexer->requestEvent(handler.get(), type);
 	}
 
 	void Dispatcher::removeHander(u::shared_ptr<AEventHandler> handler, enum EventType type) {
-		(void)type;
 		const handle_t handle = handler->getHandle();
-		_handlers.erase(handle);
-		// delete handler;
+		if (this->_handlers.find(handle) != this->_handlers.end()) {
+			const size_t index = this->_handlerIndices[handler];
+			if (index != this->_handlers[handle].size() - 1) {
+				std::swap(this->_handlers[handle][index], this->_handlers[handle].back());
+				this->_handlerIndices[this->_handlers[handle][index]] = index;
+			}
+			this->_demultiplexer->unRequestEvent(this->_handlers[handle].back().get(), type);
+			this->_handlers[handle].pop_back();
+			this->_handlerIndices.erase(handler);
+		}
 	}
 
 	void Dispatcher::handleEvent(void) {
--- src/Reactor/Dispatcher/Dispatcher.hpp
+++ src/Reactor/Dispatcher/Dispatcher.hpp
@@ -9,8 +9,8 @@ namespace reactor {
 	class Dispatcher : public utils::TSingleton<Dispatcher> {
 	   private:
 		SyncEventDemultiplexer* _demultiplexer;
-		std::map<fd_t, u::shared_ptr<AEventHandler> > _handlers;
-		// std::map<u::shared_ptr<AEventHandler>, int> _handlerIndices;
+		std::map<fd_t, std::vector<u::shared_ptr<AEventHandler> > > _handlers;
+		std::map<u::shared_ptr<AEventHandler>, size_t> _handlerIndices;
 
 		Dispatcher(const Dispatcher& obj);
 		Dispatcher& operator=(const Dispatcher& obj);
