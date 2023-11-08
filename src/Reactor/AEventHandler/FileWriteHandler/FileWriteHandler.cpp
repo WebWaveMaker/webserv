88867ab15198cef56815510ee04dee65c667c9d1
jgo-42 <jingmuio@gmail.com>
2023-11-08 16:53:04 +0900

    [WWM-87] fix(FileWriteHandler); fix file fd -> File Stream
    file fd의 동작을 명시하기 위해 fd를 FILE* file stream을 사용하여
    가독성을 높였습니다.

--- src/Reactor/AEventHandler/FileWriteHandler/FileWriteHandler.cpp
+++ src/Reactor/AEventHandler/FileWriteHandler/FileWriteHandler.cpp
@@ -1,17 +1,20 @@
 #include "FileWriteHandler.hpp"
 
 namespace reactor {
-	FileWriteHandler::FileWriteHandler(sharedData_t& sharedData) : AEventHandler(sharedData) {}
+	FileWriteHandler::FileWriteHandler(sharedData_t& sharedData)
+		: AEventHandler(sharedData), _fp(fdopen(this->getHandle(), "w")) {}
 
-	FileWriteHandler::~FileWriteHandler() {}
+	FileWriteHandler::~FileWriteHandler() {
+		fclose(this->_fp);
+	}
 
 	void FileWriteHandler::handleEvent() {
 		if (this->getState() == TERMINATE || this->getState() == RESOLVE ||
 			this->getBuffer().size() == 0)	// executeHandler가 이 핸들러의 상태를 변경한다.
 			return;
 		std::vector<char>& buffer = this->getBuffer();
-		ssize_t numberOfBytes = write(this->getHandle(), buffer.data(), this->getBuffer().size());
-		if (numberOfBytes == -1) {
+		std::size_t numberOfBytes = fwrite(buffer.data(), sizeof(char), buffer.size(), this->_fp);
+		if (ferror(this->_fp)) {
 			ErrorLogger::systemCallError(__FILE__, __LINE__, __func__, "write failed");
 			this->setState(TERMINATE);
 			return;
